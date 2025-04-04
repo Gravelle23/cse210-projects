905657d3a4092a72e4afec6dec681615fdbebd31
Aaron Gravelle <aaron.gravelle2001@gmail.com>
2025-04-04 01:09:14 -0400

    Commit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("John Doe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment a2 = new MathAssignment("Walter Evans", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment a3 = new WritingAssignment("Martha Stewart", "European History", "The cause of WWII");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+        Console.WriteLine("");
     }
+
 }
