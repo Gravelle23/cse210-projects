515dd1eb7174b44aeea0e90d635514af36b6adf6
Aaron Gravelle <aaron.gravelle2001@gmail.com>
2025-04-05 23:25:54 -0400

    commit

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -4,11 +4,13 @@ public class BreathingActivity
 {
     private string _name;
     private string _description;
+    private string _endDescription;
     private int _duration;
     public BreathingActivity()
     {
         _name = "Breathing";
         _description = "This activity will help you relax by breathing deeply.";
+        _endDescription = "Thank you for taking the time to do this Activity!";
         _duration = 60;
     }
 
@@ -19,7 +21,7 @@ public class BreathingActivity
 
     public void DisplayEndingMessage()
     {
-        Console.WriteLine($"Ending {_name}: {_description}");
+        Console.WriteLine($"Ending {_name}: {_endDescription}");
     }
 
     public void ShowSpinner(int seconds)
