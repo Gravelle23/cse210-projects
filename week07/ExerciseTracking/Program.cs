e948f3e327389e2abfa03611808a5be208637b0b
Aaron Gravelle <aaron.gravelle2001@gmail.com>
2025-04-15 22:08:00 -0400

    commit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running { Date = "03 Nov 2022", Duration = 30, Distance = 3.0 });
+
+        activities.Add(new Cycling { Date = "04 Nov 2022", Duration = 45, Speed = 12.0 });
+
+        activities.Add(new Swimming { Date = "05 Nov 2022", Duration = 40, Laps = 30 });
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
